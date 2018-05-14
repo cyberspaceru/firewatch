bc628aa6b934d902063f05d060de095fed30febf
itatsiy <iotatsiy@gmail.com>
2018-05-14 13:20:38 +0300

    Improve logging of strategy

--- src/main/java/com/wiley/firewatch/strategies/BaseAssertStrategy.java
+++ src/main/java/com/wiley/firewatch/strategies/BaseAssertStrategy.java
@@ -2,9 +2,11 @@ package com.wiley.firewatch.strategies;
 
 import com.wiley.firewatch.api.enities.ProcessingEntries;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.text.StrBuilder;
 import org.testng.Assert;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by itatsiy on 4/24/2018.
@@ -22,7 +24,10 @@ public class BaseAssertStrategy implements IAssertStrategy {
     public void execute(List<ProcessingEntries> processing) {
         for (ProcessingEntries entries : processing) {
             if (!entries.finished()) {
-                Assert.fail(errorMessage);
+                StrBuilder details = new StrBuilder();
+                Optional.ofNullable(entries.firewatchRequest()).ifPresent(x -> details.append("\n").append(x.toString()));
+                Optional.ofNullable(entries.firewatchResponse()).ifPresent(x -> details.append("\n").append(x.toString()));
+                Assert.fail(errorMessage + details.toString());
             }
         }
     }
